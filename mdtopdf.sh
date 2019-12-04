928666c6ee434e3d6a9374393a1c70071facf571
Khue Doan <k.doan@linkbynet.com>
2019-12-04 17:52:25 +0700

    Initial commit

---
+++ mdtopdf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+markdown=$(cat "$1")
+
+echo '<link rel="stylesheet" href="http://blog.yuuko.cn/markdown.css/public/styles/github/markdown.css">' > temp.html
+echo '<meta charset="UTF-8">' >> temp.html
+echo '<div class="markdown-body">' >> temp.html
+
+curl https://api.github.com/markdown/raw -X "POST" -H "Content-Type: text/plain" -d "$markdown" >> temp.html
+
+echo '</div>' >> temp.html
+
+wkhtmltopdf \
+    --margin-bottom 20 \
+    --margin-left   20 \
+    --margin-right  20 \
+    --margin-top    10 \
+    --page-size     A4 \
+    temp.html final.pdf
+
+rm temp.html
