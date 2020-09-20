632549eff1021d4ffc321000cf3f4b74234b8c9a
Khue Doan <khuedoan98@gmail.com>
2020-09-20 23:14:33 +0700

    Add tag to include CSS

--- main.go
+++ main.go
@@ -32,9 +32,15 @@ func main() {
 
     var buf bytes.Buffer
 
+    buf.WriteString("<link rel=\"stylesheet\" href=\"./markdown.css\">\n")
+    buf.WriteString("<meta charset=\"UTF-8\">\n")
+    buf.WriteString("<div class=\"markdown-body\">\n")
+
     if err := md.Convert(markdown, &buf); err != nil {
         panic(err)
     }
+
+    buf.WriteString("</div>")
 
     fmt.Print(buf.String())
 }
