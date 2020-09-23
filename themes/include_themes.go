0bfda9832c0121929180f4266551e227643679c3
Khue Doan <khuedoan98@gmail.com>
2020-09-23 23:40:41 +0700

    Include theme using go generate

--- themes/include_themes.go
+++ themes/include_themes.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
 func main() {
 	out, _ := os.Create("themes.go")
-	out.Write([]byte("package main\n\nconst (\n"))
-
-	github_css, err := ioutil.ReadFile("themes/github.css")
-	if err != nil {
-        panic(err)
-    }
-	fmt.Print(github_css)
-
-	// for _, f := range fs {
-	// 	if strings.HasSuffix(f.Name(), ".txt") {
-	// 		out.Write([]byte(strings.TrimSuffix(f.Name(), ".txt") + " = `"))
-	// 		f, _ := os.Open(f.Name())
-	// 		io.Copy(out, f)
-	// 		out.Write([]byte("`\n"))
-	// 	}
-	// }
-	out.Write([]byte(")\n"))
+	out.Write([]byte("package main\n\n"))
+	out.Write([]byte("const github_css = `\n"))
+	f, _ := os.Open("themes/github.css")
+	io.Copy(out, f)
+	out.Write([]byte("`\n"))
 }
